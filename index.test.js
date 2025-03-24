ca02dd8615fb9b280c72a324c4cfb6d59bbcd944
btanmoy <tanmoy@artisanssolutions.com>
2025-03-24 18:43:06 +0530

    added the funtion and test cases

--- index.test.js
+++ index.test.js
@@ -1,4 +1,16 @@
-//  testing
+//  test
 
 const add = require('./index');
+
+test('returns 0 for an empty string', () => {
+    expect(add("")).toBe(0);
+});
+
+test('returns number itself if only one number is provided', () => {
+    expect(add("1")).toBe(1);
+});
+
+test('adds two comma-separated numbers', () => {
+    expect(add("1,5")).toBe(6);
+});
 
